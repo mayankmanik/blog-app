9aaf7be92f02eeaa19d0497ae10626d39531af80
Mayank Manik <mayank18jha@gmail.com>
2023-09-19 17:35:45 +0530

    implemented dark mode

--- src/app/components/themeToggle/ThemeToggle.jsx
+++ src/app/components/themeToggle/ThemeToggle.jsx
@@ -1,8 +1,34 @@
-import React from 'react';
+'use client';
+import React, { useContext } from 'react';
 import styles from './themeToggle.module.css';
+import Image from 'next/image';
+import { ThemeContext } from '@/context/ThemeContext';
 
 const ThemeToggle = () => {
-  return <div className={styles.container}>ThemeToggle</div>;
+  const { toggle, theme } = useContext(ThemeContext);
+
+  return (
+    <div
+      className={styles.container}
+      onClick={toggle}
+      style={
+        theme === 'dark'
+          ? { backgroundColor: 'white' }
+          : { backgroundColor: '#0f172a' }
+      }
+    >
+      <Image src="/moon.png" alt="switch" width={14} height={14} />
+      <div
+        className={styles.ball}
+        style={
+          theme === 'dark'
+            ? { left: 2, background: '#0f172a' }
+            : { right: 2, background: 'white' }
+        }
+      ></div>
+      <Image src="/sun.png" alt="switch" width={14} height={14} />
+    </div>
+  );
 };
 
 export default ThemeToggle;
