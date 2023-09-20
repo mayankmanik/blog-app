3a22901f9afbf9f74a685cded9eb3246814c8037
Mayank Manik <mayank18jha@gmail.com>
2023-09-20 23:57:56 +0530

    implemented hamburger menu

--- src/app/components/authLinks/AuthLinks.jsx
+++ src/app/components/authLinks/AuthLinks.jsx
@@ -1,18 +1,43 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 import styles from './authLinks.module.css';
 import Link from 'next/link';
 
 const AuthLinks = () => {
+  const [open, setOpen] = useState(false);
+
   const status = 'authenticated';
   return (
     <>
       {status === 'authenticated' ? (
-        <Link href="/login">Login</Link>
+        <Link href="/login" className={styles.link}>
+          Login
+        </Link>
       ) : (
         <>
           <Link href="/write">Write</Link>
           <span className={styles.link}>Logout</span>
         </>
+      )}
+      <div className={styles.burger} onClick={() => setOpen(!open)}>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+      </div>
+      {open && (
+        <div className={styles.responsiveMenu}>
+          <Link href="/">Homepage</Link>
+          <Link href="/">Contact</Link>
+          <Link href="/">About</Link>
+          {status === 'authenticated' ? (
+            <Link href="/login">Login</Link>
+          ) : (
+            <>
+              <Link href="/write">Write</Link>
+              <span className={styles.link}>Logout</span>
+            </>
+          )}
+        </div>
       )}
     </>
   );
